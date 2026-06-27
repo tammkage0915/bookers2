084d0b85f9acb4c4fbbf965285b59f869ebff4fb
tammkage0915 <280695020+tammkage0915@users.noreply.github.com>
2026-06-27 04:43:07 +0000

    [making]bookers2_authentication1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def after_logout_url
+    root_url
+  end
 end
