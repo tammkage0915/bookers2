a7a3adab2303561e12418c65c04110010a315a21
tammkage0915 <280695020+tammkage0915@users.noreply.github.com>
2026-06-27 05:42:37 +0000

    [making]bookers2_authentication2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_06_27_031609) do
+ActiveRecord::Schema[8.0].define(version: 2026_06_27_054112) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_27_031609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.text "introduction"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
